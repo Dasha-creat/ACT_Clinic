24d9cfe594b3f26de57fa4360afa258e7da9a8d9
i22s0910 <i22s0910@students.it-college.ru>
2025-03-14 23:03:18 +0500

    slider-block almost done

---
+++ test_task/src/shared/constants/BeforeAfterText/beforeAfterText.js
@@ -0,0 +1,33 @@
+import { first_before, first_after, second_before, second_after, third_before, third_after } from "../../assets"
+
+export const BEFORE_AFTER = [
+    {
+        id: "1",
+        difference: {
+            before: first_before,
+            after: first_after
+        },
+        subtitle: "Имплантация"
+    },
+    {
+        id: "2",
+        difference: {
+            before: second_before,
+            after: second_after
+        },
+        subtitle: "Имплантация"
+    },
+    {
+        id: "3",
+        difference: {
+            before: third_before,
+            after: third_after
+        },
+        subtitle: "Имплантация"
+    },
+]
+
+export const BEFORE_AFTER_TEXT = {
+    title: "Оцените наши результаты",
+    subtitle: "Не призываем верить на слово!"
+}
